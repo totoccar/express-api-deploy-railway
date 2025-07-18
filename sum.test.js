0b93f0489f4e3a6ade0ed44e3296b558fa013375
Antonio <totocarlos0@outlook.com>
2025-07-18 11:58:07 -0300

    Prueba error en los tests

--- sum.test.js
+++ sum.test.js
@@ -5,3 +5,12 @@ function sum(a, b) {
 test('suma 1 + 2 para dar 3', () => {
     expect(sum(1, 2)).toBe(3);
 });
+
+//Funcion resta mal hecha
+function resta(a, b) {
+    return a - b - 1;
+}
+
+test('resta 5 - 2 para dar 3', () => {
+    expect(resta(5, 2)).toBe(3);
+});
